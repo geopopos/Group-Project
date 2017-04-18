72536741816662b417cd5131beeafc087f68fd93
Steven <steven@Stevens-MacBook-Pro.local>
2017-04-18 15:15:50 -0400

    Initial Commit

---
+++ ScheduleInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScheduleInfo
+//
+//  Created by Steven on 4/18/17.
+//  Copyright © 2017 Steven Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
